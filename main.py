ed8e234e89920a50119bb684e8553bd4b606a810
khairullovruslan <khairullov.ruslan@gmail.com>
2022-02-23 15:34:56 +0300

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,  render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 image = None
@@ -7,6 +7,16 @@ image = None
 @app.route('/training/<prof>')
 def index(prof):
     return render_template('base.html', title=prof)
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    profes = ["инженер-исследователь", "пилот", "строитель", "экзобиолог", "врач",
+                       "инженер по терраформированию",
+                       "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог",
+                       "инженер жизнеобеспечения", " метеоролог", "оператор марсохода", "киберинженер", "штурман",
+                       "пилот дронов"]
+    return render_template('base2.html', form=list, profs=profes)
 
 
 if __name__ == '__main__':
